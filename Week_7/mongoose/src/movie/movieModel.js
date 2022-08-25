dd4394194adeebe0f4aba34d443835c26398e9e1
Phillip Girdlestone <PhillipGirdlestone@mail.com>
2022-08-25 17:10:33 +0100

    Updated mongoose folder. Added more crud operations

--- Week_7/mongoose/src/movie/movieModel.js
+++ Week_7/mongoose/src/movie/movieModel.js
@@ -28,3 +28,4 @@ module.exports = Movie
 //Everything in Mongoose starts with a Schema. 
 //Each schema maps to a MongoDB collection and 
 //defines the shape of the documents within that collection.
+
